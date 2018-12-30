a3c76a7e494273ec65c8ea10255953566c401371
sufianamara79 <sufianamara92@gmail.com>
2018-12-30 18:31:46 +0100

    selenium

--- src/test/java/com/google/qa/testcases/MyTest.java
+++ src/test/java/com/google/qa/testcases/MyTest.java
@@ -2,13 +2,30 @@ package com.google.qa.testcases;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class MyTest {
+import com.google.qa.base.TestBase;
+import com.google.qa.pages.HomePage;
 
+public class MyTest extends TestBase{
+
+	public MyTest() {
+		super();
+		
+		
+	}
+	
+	@Before
+	public void SetUp() {
+		init();
+		
+		
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		HomePage page = new HomePage();
 	}
 
 }
