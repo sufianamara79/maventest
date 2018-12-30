52bbd3be8a0299c523675842c3d4c6a4afc476e5
sufianamara79 <sufianamara92@gmail.com>
2018-12-30 18:24:23 +0100

    test

---
+++ src/test/java/com/google/qa/testcases/MyTest.java
@@ -0,0 +1,14 @@
+package com.google.qa.testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
