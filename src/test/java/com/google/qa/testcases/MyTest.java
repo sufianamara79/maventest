a438912b2a332817ca57212125d7aaaaefcf17f0
sufianamara79 <sufianamara92@gmail.com>
2019-01-01 20:12:27 +0100

    resources

--- src/test/java/com/google/qa/testcases/MyTest.java
+++ src/test/java/com/google/qa/testcases/MyTest.java
@@ -33,7 +33,7 @@ public class MyTest extends TestBase{
 		page.Search("real madrid");
 		
 		LuxDetails details = new LuxDetails();
-		details.setTeamID(5);
+		details.setTeamID(6);
 		details.setName("real");
 		details.setRank(1);
 		
